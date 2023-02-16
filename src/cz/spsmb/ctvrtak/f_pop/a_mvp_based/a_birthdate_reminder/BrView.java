fc951d9db4947fb4d05aef1e55bd17664a0c5d6f
Hanis03 <spoluprace.vitek@seznam.cz>
2023-02-16 15:35:29 +0100

    updated BrView

--- src/cz/spsmb/ctvrtak/f_pop/a_mvp_based/a_birthdate_reminder/BrView.java
+++ src/cz/spsmb/ctvrtak/f_pop/a_mvp_based/a_birthdate_reminder/BrView.java
@@ -3,16 +3,14 @@ package cz.spsmb.ctvrtak.f_pop.a_mvp_based.a_birthdate_reminder;
 import cz.spsmb.ctvrtak.f_pop.a_mvp_based.a_birthdate_reminder.model.HumanRepository;
 import cz.spsmb.ctvrtak.f_pop.a_mvp_based.a_birthdate_reminder.model.Human;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.DateCell;
 import javafx.scene.control.DatePicker;
-import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
-import javafx.stage.Screen;
 import javafx.stage.Stage;
-import javafx.util.Callback;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.time.LocalDate;
@@ -34,15 +32,39 @@ public class BrView extends Application {
         this.dp.setValue(this.date);
         this.mainBp.setTop(this.dp);
         this.mainBp.setCenter(this.tf);
-        //this.customizeDp();
-        for (Human h:model.getAll()) {
-            this.tf.getChildren().add(new Text(h.getFirstName()+" "+h.getSecondName()+String.format("(%s)", h.getBirthdate())));
-            this.tf.getChildren().add(new Text("\n"));
-        }
 
+        //this.customizeDp();
+        /*for (Human h : model.getAll()) {
+            for (int i = 0; i <= 7; i++){
+                this.tf.getChildren().add(new Text(h.getFirstName() + " " + h.getSecondName() + String.format("(%s)", h.getBirthdate())));
+            this.tf.getChildren().add(new Text("\n"));
+            }
+        }*/
+
+        setHandlers();
         stage.setScene(this.scene);
         stage.show();
 
+    }
+    public void setHandlers(){
+        this.dp.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                BrView.this.tf.getChildren().clear();
+                BrView.this.date = BrView.this.dp.getValue();
+                for (int i = 0; i <= 7; i++){
+                    LocalDate tmp = BrView.this.date.plusDays(i);
+                    BrView.this.tf.getChildren().add(new Text("\n" + tmp.getDayOfWeek() + " " + tmp + " "));
+                    for (Human h : model.getAll()){
+                        if (h.getBirthdate().getMonth() == tmp.getMonth() && h.getBirthdate().getDayOfMonth() == tmp.getDayOfMonth()){
+                            int vek = tmp.getYear() - h.getBirthdate().getYear();
+                            BrView.this.tf.getChildren().add(new Text(h.getFirstName() + " " + h.getSecondName() + " - slaví " + vek + "té narozeniny"));
+
+                        }
+                    }
+                }
+            }
+        });
     }
 
     public static void main(String[] args) {
--- src/cz/spsmb/ctvrtak/f_pop/a_mvp_based/a_birthdate_reminder/resources/data/data.sql
+++ src/cz/spsmb/ctvrtak/f_pop/a_mvp_based/a_birthdate_reminder/resources/data/data.sql
@@ -1,4 +1,6 @@
 INSERT INTO human (id, email, firstName, secondName, birthdate) VALUES
 (1, 'as@ff.com', 'Dan', 'Kolář', '2001-01-01'),
 (2, 'aadss@fsdf.com', 'Pavel', 'Fíkovníků', '1900-01-01'),
-(3, 'aadssdfs@fsdf.com', 'Petr', 'Pavel', '1212-07-07');
+(3, 'aadssdfs@fsdf.com', 'Petr', 'Pavel', '1212-07-07'),
+(4, 'test@test.ts','Test','Test','2013-02-17'),
+(5, 'test@test.ts','Test','Test','2003-02-18');
